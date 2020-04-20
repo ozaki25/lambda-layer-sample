f52d7b3ecc191a2d1574c5fd05745a3a0f74b342
ozaki <fs65ugumori@yahoo.co.jp>
2020-04-21 00:12:55 +0900

    update handler

--- handler.js
+++ handler.js
@@ -1,4 +1,36 @@
+import {
+  puppeteer,
+  args,
+  defaultViewport,
+  executablePath,
+  headless,
+} from 'chrome-aws-lambda';
+
 export const hello = async event => {
+  let browser = null;
+
+  try {
+    browser = await puppeteer.launch({
+      args,
+      defaultViewport,
+      executablePath: await executablePath,
+      headless,
+    });
+
+    const page = await browser.newPage();
+    await page.goto(event.url || 'https://google.com');
+
+    const result = await page.title();
+    console.log({ result });
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: error.message }),
+    };
+  } finally {
+    if (browser !== null) await browser.close();
+  }
+
   return {
     statusCode: 200,
     body: JSON.stringify({ message: 'Hello' }),
