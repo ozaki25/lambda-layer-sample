30905dc23308abc0762247d5417ea0228899c4ef
ozaki <fs65ugumori@yahoo.co.jp>
2020-04-21 01:03:15 +0900

    remove webpack-node-externals

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const path = require('path');
 // eslint-disable-next-line import/no-unresolved
 const slsw = require('serverless-webpack');
-const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
   entry: slsw.lib.entries,
@@ -12,7 +11,6 @@ module.exports = {
   },
   mode: 'development',
   target: 'node',
-  externals: [nodeExternals()],
   module: {
     rules: [
       {
