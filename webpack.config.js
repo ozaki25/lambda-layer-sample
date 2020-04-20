d0cc97a6df9cbb0c7653c6cf4356662b53a9db97
ozaki <fs65ugumori@yahoo.co.jp>
2020-04-21 01:02:30 +0900

    add node externals config

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 // eslint-disable-next-line import/no-unresolved
 const slsw = require('serverless-webpack');
+const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
   entry: slsw.lib.entries,
@@ -11,6 +12,7 @@ module.exports = {
   },
   mode: 'development',
   target: 'node',
+  externals: [nodeExternals()],
   module: {
     rules: [
       {
